8f5ff537761715ffa95b7da30c470991c3badb3e
Joyboy97 <winnerstar29@gmail.com>
2024-01-24 02:43:59 -0500

    lets ago

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fix1 from "../src/images/deploy-predeploy.png";
+import fix2 from "../src/images/gh-pages.png"
+import fix3 from "../src/images/homepage.png"
 
 function App() {
   return (
@@ -9,7 +12,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          MAKE SURE YOU MAKE 
         </p>
+        <div className="container">
+          <img src={fix1} alt="fix 1"/>
+          <p>make sure that there are deploy directions</p>
+          <img src={fix2} alt="fix 2"/>
+          <p>make sure to install githuub pages</p>
+          <img src={fix3} alt="fix 3"/>
+          <p>make sure your home page is correct</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
